7e169c122606d885cb91d3aa44603e8e8b826805
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-01-18 21:43:57 +0300

    Reseeded Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -110,15 +110,15 @@ puts "Done Seeding Clients"
 
 
 puts "Seeding Venues....."
-30.times do 
+
     Venue.create!([
 
   {
-    price: 10000,
+    price: 1000,
   description: Faker::Lorem.paragraph,
   location: "Nairobi",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/708261478917481187/",
+  imageUrl: "https://media.istockphoto.com/id/1029195356/photo/wedding-day-decoration-for-a-outside-wedding.jpg?s=612x612&w=0&k=20&c=RV_KGz6asDO5af12ghMS2TNlvO6xHfN3CBjyFO8fxQI=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -128,7 +128,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Mombasa",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/161214861655101531/",
+  imageUrl: "https://i.pinimg.com/236x/b6/a6/6e/b6a66e67b0906a0b6215da990c2a895e.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -138,7 +138,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Desmond Tutu",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/211174975171735/",
+  imageUrl: "https://i.pinimg.com/236x/9a/81/6c/9a816c7790d21e06ad76edbef0296416.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -148,7 +148,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Merica",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/383509724529212900/",
+  imageUrl: "https://i.pinimg.com/236x/c7/94/86/c7948659a4265185110a839fe7f00432.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -158,7 +158,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Nakuru",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/1017039528323605108/"  ,
+  imageUrl: "https://i.pinimg.com/236x/ba/8d/1a/ba8d1a70bbf58147e933cef425c1e6de.jpg"  ,
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -168,27 +168,27 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Buraha Zenoni",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/507710558004414104/",
+  imageUrl: "https://i.pinimg.com/236x/01/90/29/019029cdeb1a0abb2e910228a95b4773.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 10010,
+    price: 10000,
   description: Faker::Lorem.paragraph,
   location: "GrandWinston",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/708261478917481187/",
+  imageUrl: "https://i.pinimg.com/236x/02/f9/fd/02f9fde573060c168850cf3365ce5dc5.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 1005880,
+    price: 10080,
   description: Faker::Lorem.paragraph,
   location: "The Emory",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/5348093296837619/"  ,
+  imageUrl: "https://i.pinimg.com/236x/df/24/38/df243872ec9d2a4574127a200300ebc2.jpg"  ,
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -198,87 +198,87 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "NuruPalace",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/395753886017160086/"  ,
+  imageUrl: "https://i.pinimg.com/236x/01/54/0e/01540e59e15f5a76fc02eac2dcbb5d90.jpg"  ,
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 100890,
+    price: 1500,
   description: Faker::Lorem.paragraph,
   location: "Eldoret",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/87749892732217631/",
+  imageUrl: "https://media.istockphoto.com/id/1097270470/photo/floral-arrangement-decoration-for-the-backdrop-of-wedding-arch.webp?s=612x612&w=is&k=20&c=oYJJCmgpaHXIdbs5s43SlmGP2xepm3Fl2mT_SvkK5OI=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 100000,
+    price: 10000,
   description: Faker::Lorem.paragraph,
   location: "Kakamega",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/103723597659532070/"  ,
+  imageUrl: "https://media.istockphoto.com/id/472999512/photo/footpath-in-a-botanical-garden-with-orchids-lining-the-path.jpg?s=1024x1024&w=is&k=20&c=O9LzkZQ4I2RHbHLNMPJGn41DUWLgpUaYhrsXuus4J_Y=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 105400,
+    price: 5400,
   description: Faker::Lorem.paragraph,
   location: "Kiambu",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/116178865373536458/"  ,
+  imageUrl: "https://i.pinimg.com/originals/d0/ef/e7/d0efe7349982bdea97a74d03787a6e05.jpg"  ,
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 10050,
+    price: 1450,
   description: Faker::Lorem.paragraph,
   location: "Milimami APart",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/622693085984499573/ ",
+  imageUrl: "https://media.istockphoto.com/id/516843392/photo/wedding-banquet-in-garden.jpg?b=1&s=170667a&w=0&k=20&c=9OUTzD0iDNryxdJOxpRwvKt-vBpjvKvTWL91kOmn-W0=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 14000,
+    price: 1400,
   description: Faker::Lorem.paragraph,
   location: "Thika",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/155303887533749094/",
+  imageUrl: "https://media.istockphoto.com/id/1365834463/photo/chairs-in-front-of-a-wedding-altar-on-a-background-of-green-trees-and-grass-wedding-ceremony.jpg?b=1&s=170667a&w=0&k=20&c=FsLNwsZZgPavrtG7crywtl_3I66JXVGjWm9fUYgK0Po=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 14000,
+    price: 14500,
   description: Faker::Lorem.paragraph,
   location: "Kisumu",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/449937819034352088/",
+  imageUrl: "https://media.istockphoto.com/id/89352097/photo/garden-wedding-chairs-and-flowers.jpg?s=1024x1024&w=is&k=20&c=TBr9KYjMCmKCMPtC41BFXO_a7-dAVIgoFRJI5GnH7ls=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 10400,
+    price: 10500,
   description: Faker::Lorem.paragraph,
   location: "Kileleshwa",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/303359724911963707/",
+  imageUrl: "https://media.istockphoto.com/id/147953641/photo/wedding-veranda-in-hotel.jpg?s=612x612&w=is&k=20&c=icj4KOnj2lbmhft-O16Z1mo8byUnTAKl6pYaAQOz3Kc=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 10040,
+    price: 20000,
   description: Faker::Lorem.paragraph,
   location: "Lamu",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/358388082858960323/",
+  imageUrl: "hhttps://images.unsplash.com/photo-1557296440-0dc5e8ba9bc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -288,7 +288,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Malindi",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/304907837270619055/",
+  imageUrl: "https://i.pinimg.com/564x/e5/0f/6d/e50f6db8dfd3f068f269fb11e6468c3b.jpg",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -298,7 +298,7 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Kwale",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/341288477985566046/",
+  imageUrl: "https://images.unsplash.com/photo-1629243623657-17431c65be8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
@@ -308,22 +308,22 @@ puts "Seeding Venues....."
   description: Faker::Lorem.paragraph,
   location: "Taita Taveta",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/144959681741921800/",
+  imageUrl: "https://plus.unsplash.com/premium_photo-1664471481659-bc3c4cedab70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   },
 
   {
-    price: 10030,
+    price: 6000,
   description: Faker::Lorem.paragraph,
   location: "Githurai",
   capacity: Faker::Number.between(from: 10, to: 300),
-  imageUrl: "https://www.pinterest.com/pin/198580664809473816/",
+  imageUrl: "https://media.istockphoto.com/id/1408017316/photo/beautiful-wedding-decoration-the-area-of-the-wedding-ceremony-an-arch-of-flowers-golden.jpg?s=1024x1024&w=is&k=20&c=YLHdDpPGbSSxGr-Q6OD4KRMP_LytNrToL3YBR7IgGvM=",
   client_id: rand(1..Client.all.size),
   manager_id: rand(1..Manager.all.size)
   }
 ])
-end
+
 puts "Done Seeding Venues"
 
 
